a12f2ab2290b465748f60972dd339337bb0ba2aa
jway <james.way@lookout.com>
2015-01-27 13:56:41 -0800

    remove config from collector.sh

--- zipkin-collector-service/src/scripts/collector.sh
+++ zipkin-collector-service/src/scripts/collector.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VERSION=@VERSION@
-CONFIG=${DIR}/../config/collector-dev.scala
-java -cp "$DIR/../libs/*" -jar $DIR/../zipkin-collector-service-$VERSION.jar -f $CONFIG
+java -cp "$DIR/../libs/*" -jar $DIR/../zipkin-collector-service-$VERSION.jar
